1e7929b7e3a2964ca14fbf98e9f4d449a5254d00
spatte22 <sean.patterson1989@gmail.com>
2021-02-14 13:44:35 -0700

    working on GSAP stuff

--- mainJS.js
+++ mainJS.js
@@ -1,7 +1,7 @@
 const timelineButton = document.getElementsByClassName('timelineButton');
 const buttonFill = document.getElementsByClassName('buttonBlank');
-let a;
-for (a=0; a < timelineButton.length; a++) {
+
+for (let a=0; a < timelineButton.length; a++) {
 		timelineButton[a].onclick = function() {
 			this.nextElementSibling.classList.toggle('clock');
 			this.firstElementChild.classList.toggle('buttonFill');
